ad4dfff9a94fdfa61062253a0c9f278795ab8d62
Leonardo Keiser Fleck <89053412+Leonardo-KF@users.noreply.github.com>
2022-08-01 22:58:41 -0300

    create base structure

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+let animes = require("./mocks/animes");
+
+app.get("/", function (req, res) {
+  console.log(req.headers);
+  res.send(animes);
+});
+
+// app.get("/raiz/:number", function (req, res) {
+//   let number = req.params.number;
+//   const result = number ** 0.5;
+//   if (result % 1 === 0) {
+//     res.send({
+//       message: "Raiz quadrada do numero " + number,
+//       result: `${result}`,
+//     });
+//   } else {
+//     res.send({
+//       message: "O numero " + number + " não possui raiz exata",
+//     });
+//   }
+// });
+
+app.get("/anime/:id", function (req, res) {
+  const animeId = req.params.id;
+  let animeFinded;
+
+  animes.map((anime) => {
+    if (anime.id === animeId) {
+      animeFinded = anime;
+    }
+  });
+  if (animeFinded) {
+    res.send(animeFinded);
+  } else {
+    res.send("Nenhum anime com esse id foi encontrado");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("A aplicação esta rodando na porta 3000");
+  console.log("Acesse em: http://localhost:3000");
+});
---
+++ mocks/animes.js
@@ -0,0 +1,50 @@
+const animes = [
+  {
+    id: "2145abc",
+    title: "Naruto Shippuden",
+    protagonist: "Naruto",
+    gender: "Shonen",
+    year: 2009,
+    characters: [
+      {
+        name: "Sasuke",
+        lastName: "Uchiha",
+        skill: "sharingan",
+        age: 16,
+        gender: "Male",
+      },
+      {
+        name: "Shikamaro",
+        lastName: "Nara",
+        skill: "kagemane",
+        age: 16,
+        gender: "Male",
+      },
+    ],
+  },
+  {
+    id: "214765abc",
+    title: "Dragon Ball Z",
+    protagonist: "Goku",
+    gender: "Shonen",
+    year: 1998,
+    characters: [
+      {
+        name: "Vegeta",
+        lastName: "unknow",
+        skill: "Sayajin",
+        age: 25,
+        gender: "Male",
+      },
+      {
+        name: "Majimboo",
+        lastName: "unknow",
+        skill: "Eat",
+        age: 3000,
+        gender: "Male",
+      },
+    ],
+  },
+];
+
+module.exports = animes;
