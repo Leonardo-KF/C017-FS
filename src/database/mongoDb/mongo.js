4d8c828465777d4441848c6f3617521250c1fe88
Leonardo Keiser Fleck <89053412+Leonardo-KF@users.noreply.github.com>
2022-08-15 22:12:43 -0300

    feat: update mongo connection to deploy

--- src/database/mongoDb/mongo.js
+++ src/database/mongoDb/mongo.js
@@ -1,7 +1,10 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const { connect } = require("mongoose");
 
 function mongoConnect() {
+  console.log(process.env);
   connect(process.env.MONGO_URL ?? "mongodb://admin:admin@localhost:27020/", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
