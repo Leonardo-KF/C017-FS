059b44dc23fa4ed4a108c5a3e0171e46016f6e35
Leonardo Keiser Fleck <89053412+Leonardo-KF@users.noreply.github.com>
2022-08-17 22:32:00 -0300

    feat: implement updateUserUseCase

---
+++ services/usecases/updateUser.js
@@ -0,0 +1,23 @@
+import { UserEntity } from "../../entities/user.js";
+export class UpdateUserUseCase {
+  constructor(userRepository, findUserById) {
+    this.repository = userRepository;
+    this.findUserById = findUserById;
+  }
+  async execute(userUpdated, userId) {
+    // encontra e retorna o user por id, se não encontrar retorna undefined
+    const userToUpdate = await this.findUserById(userId);
+    // se userToUpdate for undefined dispara um erro pois não encontrou o user
+    if (!userToUpdate) {
+      throw new Error("Not found a user with Userid:" + userId);
+    }
+    // pega os valores antigos do usuário encontrado e substitui pelos valores novos que recebemos como parâmetro
+    const userModified = { ...userToUpdate, userUpdated };
+    // usa a entidade para pegar as informações do usuário para poder valida-lo
+    const userValidated = new UserEntity(userModified);
+    // se as novas propriedades não forem válidas a entidade irá disparar um erro
+    userValidated.validate();
+    // vai passar os dados do usuário que virão da entidade para o repository
+    return await this.repository.updateUser(userValidated.getUser());
+  }
+}
